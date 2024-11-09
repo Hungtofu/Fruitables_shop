54014fb9f73b40bf87454baf7d313d1bd7c10373
Hungtofu <vinhhungalex03@gmail.com>
2024-11-09 23:18:08 +0700

    first commit

---
+++ src/main/java/Fruitables/shop/FruitablesShopApplication.java
@@ -0,0 +1,13 @@
+package Fruitables.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitablesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitablesShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Fruitables/shop/FruitablesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Fruitables.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitablesShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
