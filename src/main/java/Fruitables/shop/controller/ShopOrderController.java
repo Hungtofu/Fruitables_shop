b141e3a2c5b839652530c34e7143dd5ae938d65d
Nguyen-Hoang-7 <21522100@gm.uit.edu.vn>
2024-12-09 13:58:09 +0700

    Add ShopOrderController file

---
+++ src/main/java/Fruitables/shop/controller/ShopOrderController.java
@@ -0,0 +1,13 @@
+package Fruitables.shop.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/shoporder")
+
+public class ShopOrderController {
+
+}
