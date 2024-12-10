dadde64f01ff50aa237b197950723e14c2968cae
Nguyen-Hoang-7 <21522100@gm.uit.edu.vn>
2024-12-10 16:58:53 +0700

    Create function add Product to User Cart in Shop Order Service

--- src/main/java/Fruitables/shop/service/ShopOrderService.java
+++ src/main/java/Fruitables/shop/service/ShopOrderService.java
@@ -1,14 +1,52 @@
 package Fruitables.shop.service;
 
+import Fruitables.shop.entity.*;
 import Fruitables.shop.repository.ShopOrderItemRepository;
 import Fruitables.shop.repository.ShopOrderRepository;
+import Fruitables.shop.util.ImgUtil;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class ShopOrderService {
     private final UserService userService;
     private final ShopOrderRepository shopOrderRepository;
     private final ShopOrderItemRepository shopOrderItemRepository;
+    private final ImgUtil imgUtil;
 
+    public ShopOrderService(UserService userService, ShopOrderRepository shopOrderRepository, ShopOrderItemRepository shopOrderItemRepository, ImgUtil imgUtil)
+    {
+        this.userService = userService;
+        this.shopOrderRepository = shopOrderRepository;
+        this.shopOrderItemRepository = shopOrderItemRepository;
+        this.imgUtil = imgUtil;
+    }
 
+    public ShopOrder initUserShopOrder(String userEmail)
+    {
+        User user = userService.findByEmail(userEmail);
+        ShopOrder shopOrder = shopOrderRepository.findByUser(user);
+        if (shopOrder == null)
+        {
+            ShopOrder newShopOrder = new ShopOrder();
+            newShopOrder.setUser(user);
+            shopOrderRepository.save(newShopOrder);
+            return newShopOrder;
+        }
+        return shopOrder;
+    }
+
+    public boolean addProductToUserShopOrder(String email, Product product, int qty, BigDecimal price)
+    {
+        ShopOrder shopOrder = initUserShopOrder(email);
+        ShopOrderItem shopOrderItem = new ShopOrderItem(shopOrder, product, qty, price);
+        try {
+            shopOrderRepository.save(shopOrderItem);
+            return true;
+        } catch (Exception e) {
+            System.out.print("Error save shop order item: "+ e.getMessage());
+            return false;
+        }
+    }
 }
