701c7fe1ca858a8a65344c7595572933f3a72284
Nguyen-Hoang-7 <21522100@gm.uit.edu.vn>
2024-12-22 18:42:01 +0700

    Adding the api of getting all Shipping Methods

---
+++ src/main/java/Fruitables/shop/controller/ShippingMethodController.java
@@ -0,0 +1,29 @@
+package Fruitables.shop.controller;
+
+import Fruitables.shop.payload.RestResponse;
+import Fruitables.shop.service.ShippingMethodService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/shipping_method")
+public class ShippingMethodController {
+    private final ShippingMethodService shippingMethodService;
+    public ShippingMethodController(ShippingMethodService shippingMethodService)
+    {
+        this.shippingMethodService = shippingMethodService;
+    }
+
+    @GetMapping("getall")
+    public ResponseEntity<RestResponse<Object>> getAll()
+    {
+        RestResponse<Object> response = new RestResponse<Object>();
+        response.setData(shippingMethodService.getallShippingMethod());
+        return ResponseEntity.status(HttpStatus.OK.value()).body(response);
+    }
+}
---
+++ src/main/java/Fruitables/shop/service/ShippingMethodService.java
@@ -0,0 +1,25 @@
+package Fruitables.shop.service;
+
+import Fruitables.shop.dto.ShippingMethodDTO;
+import Fruitables.shop.entity.ShippingMethod;
+import Fruitables.shop.repository.ShippingMethodRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShippingMethodService {
+    private final ShippingMethodRepository shipRepo;
+    public ShippingMethodService(ShippingMethodRepository shipRepo)
+    {
+        this.shipRepo = shipRepo;
+    }
+
+    public List<ShippingMethodDTO> getallShippingMethod() {
+        List<ShippingMethodDTO> shiplist = new ArrayList<>();
+        for (ShippingMethod s : this.shipRepo.findAll())
+        {
+            shiplist.add(new ShippingMethodDTO(s));
+        }
+        return shiplist;
+    }
+}
