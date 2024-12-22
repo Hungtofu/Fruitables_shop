a87f0ba0c3202939df6d25bcabff5d4f5d341a95
Nguyen-Hoang-7 <21522100@gm.uit.edu.vn>
2024-12-23 01:13:38 +0700

    Adding the api of Inserting User Payment Method

--- src/main/java/Fruitables/shop/dto/UserPaymentMethodDTO.java
+++ src/main/java/Fruitables/shop/dto/UserPaymentMethodDTO.java
@@ -1,14 +1,24 @@
 package Fruitables.shop.dto;
 
+import Fruitables.shop.entity.PaymentType;
+import Fruitables.shop.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.sql.Timestamp;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserPaymentMethodDTO {
-
+    private int id;
+    private User user;
+    private PaymentType paymentType;
+    private String provider;
+    private String accountNumber;
+    private Timestamp expiryDate;
+    private boolean isDefault;
 }
--- src/main/java/Fruitables/shop/repository/UserPaymentMethodRepository.java
+++ src/main/java/Fruitables/shop/repository/UserPaymentMethodRepository.java
@@ -1,10 +1,10 @@
 package Fruitables.shop.repository;
 
-import Fruitables.shop.entity.PaymentType;
+import Fruitables.shop.entity.UserPaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentTypeRepository extends JpaRepository<PaymentType, Integer> {
-    PaymentType findById(int id);
+public interface UserPaymentMethodRepository extends JpaRepository<UserPaymentMethod, Integer> {
+
 }
